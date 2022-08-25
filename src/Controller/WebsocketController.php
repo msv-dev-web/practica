28690d1940ed2c56da4e17e3a98179ce91343036
Mitrofanov Sergey <mitrofanov.s.v@effective-group.ru>
2022-08-25 16:42:08 +0300

    #

---
+++ src/Controller/WebsocketController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WebsocketController extends AbstractController
+{
+	#[Route('/websocket', name: 'websocket')]
+	public function index()
+	{
+		return $this->render('websocket/index.html.twig', [
+				'controller_name' => 'WebsocketController',
+		]);
+	}
+}
