43abaf55bf74eaa92e3fcffdedb53de561bcf1f9
Mitrofanov Sergey <mitrofanov.s.v@effective-group.ru>
2022-09-21 21:00:48 +0300

    # install xhprof

--- src/Controller/WebsocketController.php
+++ src/Controller/WebsocketController.php
@@ -12,6 +12,26 @@ class WebsocketController extends AbstractController
      */
     public function index()
     {
+        // XHPROF START
+        include_once '/usr/local/lib/php/xhprof_lib/utils/xhprof_lib.php';
+        include_once '/usr/local/lib/php/xhprof_lib/utils/xhprof_runs.php';
+        \xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
+        // XHPROF START
+
+        $var = 100;
+        for ($i=0; $i<100; $i++)
+        {
+            $var +=100;
+        }
+        $var = 100;
+
+
+        // XHPROF END
+            $xhprof_data = \xhprof_disable();
+            $xhprof_runs = new \XHProfRuns_Default();
+            $run_id = $xhprof_runs->save_run($xhprof_data, 'app');
+        // XHPROF END
+
         return $this->render('websocket/index.html.twig', []);
     }
 
